f24cfe6e95cf016dc9f659378c0a6850c355f70e
oakinyelure <oakinyelure@hotmail.com>
2016-09-29 00:04:59 -0400

    Added new Class
    Added JDBC dependency, created jdbc class, started splashscreen class

---
+++ src/main/java/DatabaseTools/ConnectJdbc.java
@@ -0,0 +1,34 @@
+package DatabaseTools;
+
+/**
+ * Created by Olusegun on 9/28/2016.
+ */
+public class ConnectJdbc  {
+    private String username;
+    private String hostname;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
