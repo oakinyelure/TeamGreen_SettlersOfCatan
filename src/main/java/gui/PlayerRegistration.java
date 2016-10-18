0cc7e0337d5dbdcc2375c5ca633fe26fd02e11ea
oakinyelure <oakinyelure@hotmail.com>
2016-10-17 22:36:45 -0400

    refactored ImageView
    Inserted imageview into an array of Object leading to short lines of
    code. Implementation should work for other objects too.

--- src/main/java/gui/PlayerRegistration.java
+++ src/main/java/gui/PlayerRegistration.java
@@ -3,16 +3,30 @@ package gui;/**
  */
 
 import javafx.application.Application;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFieldBuilder;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class PlayerRegistration extends Application {
+    String[] imgFiles = {"/images/redknight.png","/images/blueknight.png","/images/greenknight.png","/images/yellowknight.png",};
     ArrayList userNames = new ArrayList();
+    Collection <String> images = new ArrayList<>();
+    ArrayList<Object> imageViews = new ArrayList<Object>();
+    List<String>  imgFilesObject= new ArrayList<>();
+    public final int numOfPlayer = 4;
+
+
 
     public static void main(String[] args) {
         launch(args);
@@ -22,19 +36,34 @@ public class PlayerRegistration extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+
         Pane root = new Pane();
+        BorderPane bPane = new BorderPane();
+        HBox hb = new HBox(10);
+
+        hb.setPrefHeight(220.0);
+
+
         BackgroundImage bImage = new BackgroundImage(new Image("/images/playerRegistration.jpg",870,670,false,true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
         root.setBackground(new Background(bImage));
 
         GridPane content = new GridPane();
-
-        ImageView player1Image = new ImageView(new Image("images/redknight.jpg"));
-        ImageView player2Image = new ImageView(new Image("images/blueknight.jpg")); 
-        ImageView player3Image = new ImageView(new Image("images/greenknight.png"));       
-        ImageView player4Image = new ImageView(new Image("images/yellowknight.png"));
+        for(int count = 1; count > numOfPlayer; count++){
+            imageViews.add(new ImageView(new Image(imgFilesObject.get(count))));
+        }
 
 
-        root.getChildren().add(content);
+        ImageView player1Image = new ImageView(new Image("/images/redknight.png", 100,100,false,true));
+        ImageView player2Image = new ImageView(new Image("/images/blueknight.png"));
+        ImageView player3Image = new ImageView(new Image("/images/greenknight.png"));
+        ImageView player4Image = new ImageView(new Image("/images/yellowknight.png"));
+
+        bPane.setCenter(content);
+        bPane.setTop(hb);
+        content.add(player1Image, 0, 0);
+
+
+        root.getChildren().add(bPane);
 
         Scene scene = new Scene(root, 850,650);
 
@@ -44,8 +73,4 @@ public class PlayerRegistration extends Application {
         primaryStage.show();
     }
 
-    public Image playerData(){
-        Image playerAvatar = new Image("");
-        return playerAvatar;
-    }
 }
