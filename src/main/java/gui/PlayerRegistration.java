fb9e8f51369b619940918d22fbf4e26f58697832
oakinyelure <oakinyelure@hotmail.com>
2016-10-24 01:11:28 -0400

    Added stylesheet to playerRegistration class
    An attempt to depopulate the class

--- src/main/java/gui/PlayerRegistration.java
+++ src/main/java/gui/PlayerRegistration.java
@@ -5,7 +5,9 @@ package gui;/**
 import javafx.application.Application;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFieldBuilder;
 import javafx.scene.image.Image;
@@ -22,22 +24,22 @@ public class PlayerRegistration extends Application {
     String[] imgFiles = {"/images/redknight.png","/images/blueknight.png","/images/greenknight.png","/images/yellowknight.png",};
     ArrayList userNames = new ArrayList();
     Collection <String> images = new ArrayList<>();
-    ArrayList<Object> imageViews = new ArrayList<Object>();
+    ArrayList<Node> imageViews = new ArrayList<Node>();
     List<String>  imgFilesObject= new ArrayList<>();
     public final int numOfPlayer = 4;
 
 
 
     public static void main(String[] args) {
+
         launch(args);
 
     }
 
     @Override
     public void start(Stage primaryStage) {
-
-
         Pane root = new Pane();
+
         BorderPane bPane = new BorderPane();
         HBox hb = new HBox(10);
 
@@ -48,24 +50,42 @@ public class PlayerRegistration extends Application {
         root.setBackground(new Background(bImage));
 
         GridPane content = new GridPane();
-        for(int count = 1; count > numOfPlayer; count++){
-            imageViews.add(new ImageView(new Image(imgFilesObject.get(count))));
-        }
+        content.getStyleClass().add("playersCOntainer");
+        content.setHgap(180);
+        content.setVgap(15);
 
 
+
+        Label player1Label = new Label("Player1");
         ImageView player1Image = new ImageView(new Image("/images/redknight.png", 100,100,false,true));
-        ImageView player2Image = new ImageView(new Image("/images/blueknight.png"));
-        ImageView player3Image = new ImageView(new Image("/images/greenknight.png"));
-        ImageView player4Image = new ImageView(new Image("/images/yellowknight.png"));
 
+        Label player2Label = new Label("Player 2");
+        ImageView player2Image = new ImageView(new Image("/images/blueknight.png", 100, 100, false, true));
+
+        Label player3Label = new Label("player 3");
+        ImageView player3Image = new ImageView(new Image("/images/greenknight.png", 100, 100, false, true));
+
+        Label player4Label = new Label("Player 4");
+        ImageView player4Image = new ImageView(new Image("/images/yellowknight.png", 100, 100, false, true));
+
+        bPane.setMargin(content, new Insets(0,0,0,160));
         bPane.setCenter(content);
         bPane.setTop(hb);
-        content.add(player1Image, 0, 0);
+        content.add(player1Label,0, 0);
+        content.add(player1Image,0,1);
+        content.add(player2Label,1, 0);
+        content.add(player2Image,1,1);
+        content.add(player3Label,0,2);
+        content.add(player3Image,0,3);
+        content.add(player4Label,1,2);
+        content.add(player4Image,1,3);
 
 
         root.getChildren().add(bPane);
 
+
         Scene scene = new Scene(root, 850,650);
+        scene.getStylesheets().add("/stylesheet/catanstylesheet.css");
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Player Registration");
