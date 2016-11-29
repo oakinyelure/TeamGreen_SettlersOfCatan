99251a6123416a52de09aee6a748fdcc09f8df83
oakinyelure <oakinyelure@hotmail.com>
2016-11-29 02:26:10 -0500

    Completed bottombar design. Color code is successfully changed. Logic error in provious code makes wheat not visible

--- src/main/java/gui/BottomBar.java
+++ src/main/java/gui/BottomBar.java
@@ -27,7 +27,7 @@ public class BottomBar extends JPanel{
     private Timer timer;
 
     public BottomBar() {
-        setBackground(Color.ORANGE);
+        setBackground(Color.BLACK);
 
         setLayout(new GraphPaperLayout(new Dimension(GameRunner.getNumbPlayers(),12)));
 
@@ -36,12 +36,14 @@ public class BottomBar extends JPanel{
             ArrayList<KComponent> components = new ArrayList<KComponent>();
             components.add(new KComponent(new JLabel(GameRunner.getPlayer(i).getName()), new Rectangle(i,0,1,1)));
             add(components.get(0).getComponent(), components.get(0).getRectangle());
+            components.get(0).getComponent().setFont(new Font("Arial",1,20));
+            components.get(0).getComponent().setForeground(Color.WHITE);
 
 
             for (int k = 1; k <= 11; k++) {
                 components.add(new KComponent(new JLabel(""), new Rectangle(i,k,1,1)));
-                components.get(k).getComponent().setFont(new Font("Arial", 1, 20));
-                components.get(k).getComponent().setForeground(Color.WHITE);
+                components.get(k).getComponent().setFont(new Font("Arial", 1, 18));
+                //components.get(k).getComponent().setForeground(Color.WHITE);
                 add(components.get(k).getComponent(), components.get(k).getRectangle());
             }
 
@@ -79,20 +81,25 @@ public class BottomBar extends JPanel{
 
     public void updatePlayer(ArrayList<KComponent> components, Player p){
         ((JLabel)components.get(1).getComponent()).setText("Brick: " + p.getNumberResourcesType("BRICK"));
-	((JLabel)components.get(1).getComponent()).setForeground(Color.decode("#943126"));
+	        ((JLabel)components.get(1).getComponent()).setForeground(Color.decode("#943126"));
         ((JLabel)components.get(2).getComponent()).setText("Wool: " + p.getNumberResourcesType("WOOL"));
-	((JLabel)components.get(2).getComponent()).setForeground(Color.GREEN);
+	        ((JLabel)components.get(2).getComponent()).setForeground(Color.GREEN);
         ((JLabel)components.get(3).getComponent()).setText("Ore: " + p.getNumberResourcesType("ORE"));
-	((JLabel)components.get(3).getComponent()).setForeground(Color.decode("#B3B6B7"));
+	        ((JLabel)components.get(3).getComponent()).setForeground(Color.LIGHT_GRAY);
         ((JLabel)components.get(4).getComponent()).setText("Grain: " + p.getNumberResourcesType("GRAIN"));
-	((JLabel)components.get(3).getComponent()).setForeground(Color.decode("#B3B6B7"));
+	        ((JLabel)components.get(4).getComponent()).setForeground(Color.ORANGE);
         ((JLabel)components.get(5).getComponent()).setText("Lumber: " + p.getNumberResourcesType("LUMBER"));
-	((JLabel)components.get(5).getComponent()).setForeground(Color.decode("#7E5109"));
+	        ((JLabel)components.get(5).getComponent()).setForeground(Color.decode("#562318"));
         ((JLabel)components.get(6).getComponent()).setText("VP: " + p.getVictoryPoints());
+            ((JLabel)components.get(6).getComponent()).setForeground(Color.BLUE);
         ((JLabel)components.get(7).getComponent()).setText("Knight: " + p.getDevCardsType("Knight"));
+            ((JLabel)components.get(7).getComponent()).setForeground(Color.BLUE);
         ((JLabel)components.get(8).getComponent()).setText("Monopoly: " + p.getDevCardsType("Monopoly"));
+            ((JLabel)components.get(8).getComponent()).setForeground(Color.BLUE);
         ((JLabel)components.get(9).getComponent()).setText("Year of plenty: " + p.getDevCardsType("Year of plenty"));
+            ((JLabel)components.get(9).getComponent()).setForeground(Color.BLUE);
         ((JLabel)components.get(10).getComponent()).setText("Road building: " + p.getDevCardsType("Road building"));
+            ((JLabel)components.get(10).getComponent()).setForeground(Color.BLUE);
         //((JLabel)components.get(11).getComponent()).setText("Victory Point: " + p.getDevCardsType("Victory Point"));
     }
 
